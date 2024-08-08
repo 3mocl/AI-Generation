79b1b34b9d453f3cf091e65bfb53dad0988c15a8
Clive Moras <3mocl@users.noreply.github.com>
2024-08-08 19:13:29 +0530

    Style Updates

--- dalle/client/src/App.jsx
+++ dalle/client/src/App.jsx
@@ -7,7 +7,7 @@ import { Home, CreatePost } from "./pages";
 const App = () => {
   return (
     <BrowserRouter>
-      <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
+      <header className="w-full flex justify-between items-center bg-[#f5ede0] sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
         <div className="flex">
           <Link to="/">
             <img src={logo} alt="logo" className="w-28 object-contain" />
@@ -18,12 +18,12 @@ const App = () => {
         </div>
         <Link
           to="/create-post"
-          className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md"
+          className="font-inter font-medium bg-[#d69341] text-white px-4 py-2 rounded-md"
         >
           Create
         </Link>
       </header>
-      <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh - 73px)]">
+      <main className="sm:p-8 px-4 py-8 w-full bg-[#27282b] min-h-[calc(100vh - 73px)]">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/create-post" element={<CreatePost />} />
--- dalle/client/src/pages/CreatePost.jsx
+++ dalle/client/src/pages/CreatePost.jsx
@@ -79,13 +79,13 @@ const CreatePost = () => {
   return (
     <section className="max-w-7xl mx-auto">
       <div>
-        <h1 className="font-extrabold text-[#222328] text-[32px]">Create!</h1>
-        <p className="mt-2 text-[#666e75] text-[16px] max-w-500px">
+        <h1 className="font-extrabold text-[#fdcf97] text-[32px]">Create!</h1>
+        <p className="mt-2 text-[#ffffff] text-[16px] max-w-500px">
           Create imaginative and visually stunning images powered by DALL-E AI &
           share them with the community!
         </p>
       </div>
-      <form className="mt-16 max-w-3xl" onSubmit={handleSubmit}>
+      <form className="mt-8 max-w-3xl" onSubmit={handleSubmit}>
         <div className="flex flex-col gap-5">
           <FormField
             labelName="Your Name"
@@ -131,19 +131,19 @@ const CreatePost = () => {
           <button
             type="button"
             onClick={generateImage}
-            className="text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+            className="text-white bg-[#d69341] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
           >
             {generatingImg ? "Generating..." : "Generate Image"}
           </button>
         </div>
         <div className="mt-10">
-          <p className="mt-2 text-[#666e75] text-14">
+          <p className="mt-2 text-[#ffffff] text-14">
             Once you have created the image you want, you can share it in the
             community!
           </p>
           <button
             type="submit"
-            className="mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+            className="mt-3 text-white bg-[#d69341] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
           >
             {loading ? "Sharing..." : "Share!"}
           </button>
