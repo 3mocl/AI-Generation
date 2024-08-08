4f2a1a6d12490478b5fd839d29d8fc47158232e6
Clive Moras <3mocl@users.noreply.github.com>
2024-08-08 13:31:22 +0530

    Logo Updates

--- dalle/client/src/App.jsx
+++ dalle/client/src/App.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import { Link, Route, Routes, BrowserRouter } from "react-router-dom";
 
-import { logo } from "./assets";
+import { logo, logo2 } from "./assets";
 import { Home, CreatePost } from "./pages";
 
 const App = () => {
   return (
     <BrowserRouter>
       <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
-        <Link to="/">
-          <img src={logo} alt="logo" className="w-28 object-contain" />
-        </Link>
+        <div className="flex">
+          <Link to="/">
+            <img src={logo} alt="logo" className="w-28 object-contain" />
+          </Link>
+          <Link to="/">
+            <img src={logo2} alt="logo2" className="ml-2 w-28 object-contain" />
+          </Link>
+        </div>
         <Link
           to="/create-post"
           className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md"
--- dalle/client/src/assets/index.js
+++ dalle/client/src/assets/index.js
@@ -1,9 +1,12 @@
 import download from './download.png';
 import logo from './logo.svg';
 import preview from './preview.png';
+import logo2 from './logo2.svg';
+
 
 export {
   download,
   logo,
+  logo2,
   preview,
 };
